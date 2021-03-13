4f76a3711cf4a783a399d5ee92a7e8ca88ff8e9d
Jigang Li <ljg16654@sjtu.edu.cn>
2021-03-13 15:52:37 +0800

    fixed gen.sh

--- gen.sh
+++ gen.sh
@@ -4,7 +4,7 @@ OUTPUT_PATH=path.txt
 OUTPUT_SDF=custom_track.sdf
 BASE_SDF_FILE=model_single_cone.sdf
 
-test -e $BASE_SDF_FILE || echo "cannot find $BASE_SDF_FILE" & exit
+test -e $BASE_SDF_FILE || exit
 mkdir -p build
 make -k
 python3 gen_pose.py
